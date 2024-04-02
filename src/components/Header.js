14e047e3198c933c56d0e748852605ee8787502c
7vTr0x7 <7vtrox7@gmail.com>
2024-04-02 20:56:15 +0530

    Login Form

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import logo from "../utils/images/logo.png";
 
 const Header = () => {
   return (
-    <div className="absolute px-8 py-2 bg-gradient-to-b from-black">
+    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10">
       <img className="h-24" src={logo} alt="logo" />
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -6,9 +6,25 @@ const Login = () => {
   return (
     <div>
       <Header />
-      <div>
+      <div className="absolute">
         <img src={BACKGROUND_IMAGE} alt="background-img" />
       </div>
+      <form className=" absolute w-3/12 bg-black p-12 mt-36 mx-auto right-0 left-0 text-white bg-opacity-85">
+        <h1 className="font-bold text-3xl pt-4 pb-8 px-2">Sign In</h1>
+        <input
+          type="email"
+          placeholder="Email"
+          className="p-3 px-2 m-2 w-full  bg-[#333]"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-3 m-2 w-full  bg-[#333]"
+        />
+        <button className="p-4 mt-8 mb-4 mx-2  bg-[#e50914] w-full rounded-lg font-bold text-xl  ">
+          Sign In
+        </button>
+      </form>
     </div>
   );
 };
