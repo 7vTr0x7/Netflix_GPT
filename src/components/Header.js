b2d33f2cdbc613105c82c72d0bc89338bb0fc39a
7vTr0x7 <7vtrox7@gmail.com>
2024-04-03 16:33:19 +0530

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,21 @@
 import React from "react";
 import logo from "../utils/images/logo.png";
-
+import user from "../utils/images/user.png";
 const Header = () => {
+  const handleSignOut = () => {};
+
   return (
-    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10">
+    <div className="absolute w-full px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img className="h-20" src={logo} alt="logo" />
+
+      <div className="flex p-4">
+        <img className="h-12 shadow-2xl mx-5 " src={user} alt="user-icon" />
+        <button
+          className="text-white font-bold text-lg  shadow-xl bg-none"
+          onClick={handleSignOut}>
+          {"SignOut"}
+        </button>
+      </div>
     </div>
   );
 };
