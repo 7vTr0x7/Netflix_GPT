b403c5214680f301229c2f26a672c97be51c06de
7vTr0x7 <7vtrox7@gmail.com>
2024-04-03 16:38:42 +0530

    Header + signOut API

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,23 @@
 import React from "react";
 import logo from "../utils/images/logo.png";
 import user from "../utils/images/user.png";
+import { signOut } from "firebase/auth";
+import { auth } from "../utils/firebase";
+
+import { useNavigate } from "react-router-dom";
 const Header = () => {
-  const handleSignOut = () => {};
+  const navigate = useNavigate();
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        // An error happened.
+        navigate("/error");
+      });
+  };
 
   return (
     <div className="absolute w-full px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
