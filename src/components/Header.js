02c085494d2ca35c775e3e9b969f6667a5a8bee4
7vTr0x7 <7vtrox7@gmail.com>
2024-04-03 20:05:04 +0530

    Protected Routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React, { useEffect } from "react";
 import logo from "../utils/images/logo.png";
-import userIcon from "../utils/images/user.png";
-import { onAuthStateChanged, signOut } from "firebase/auth";
+import { signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
-import { useDispatch, useSelector } from "react-redux";
-
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import { onAuthStateChanged } from "firebase/auth";
+
+import { useDispatch } from "react-redux";
 import { addUser, removeUser } from "../utils/Redux/userSlice";
+
 const Header = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -40,26 +43,25 @@ const Header = () => {
     signOut(auth)
       .then(() => {})
       .catch((error) => {
-        // An error happened.
         navigate("/error");
       });
   };
 
   return (
-    <div className="absolute w-full px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
-      <img className="h-20" src={logo} alt="logo" />
+    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10 w-screen flex justify-between">
+      <img className="w-52 h-24" src={logo} alt="logo" />
 
       {user && (
-        <div className="flex p-4">
+        <div className="flex p-3">
           <img
-            className="h-12 shadow-2xl mx-5 "
-            src={userIcon}
-            alt="user-icon"
+            className="h-12 w-12 rounded-lg m-3"
+            alt="user"
+            src={user.photoURL}
           />
           <button
-            className="text-white font-bold text-lg  shadow-xl bg-none"
+            className="text-[#e50914] font-bold text-xl"
             onClick={handleSignOut}>
-            {"SignOut"}
+            Sign Out
           </button>
         </div>
       )}
