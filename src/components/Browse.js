905eba011afe43077bd9b68fd21212c47032dba5
7vTr0x7 <7vtrox7@gmail.com>
2024-04-04 17:48:50 +0530

    stored series data into redux store

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,14 +1,19 @@
 import React, { useEffect } from "react";
 import Header from "./Header";
 import { API_OPTIONS } from "./../utils/constant";
+import { useDispatch } from "react-redux";
+import { addNowAiringSeries } from "../utils/Redux/seriesSlice";
 
 const Browse = () => {
+  const dispatch = useDispatch();
+
   const nowAiringSeries = async () => {
     const data = await fetch(
       "https://api.themoviedb.org/3/trending/tv/day?language=en-US",
       API_OPTIONS
     );
     const json = await data.json();
+    dispatch(addNowAiringSeries(json.results));
     console.log(json);
   };
 
