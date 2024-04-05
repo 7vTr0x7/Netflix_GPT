2a7e40617698f90d0659886468933a81fb17cdc0
7vTr0x7 <7vtrox7@gmail.com>
2024-04-05 17:37:37 +0530

    storing trailer in redux

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,7 +1,11 @@
 import React, { useEffect } from "react";
 import { API_OPTIONS } from "./../utils/constant";
+import { useDispatch } from "react-redux";
+import { addTrailerVideo } from "../utils/Redux/seriesSlice";
 
 const VideoBackground = ({ seriesId }) => {
+  const dispatch = useDispatch();
+
   const getSeriesVideo = async () => {
     const data = await fetch(
       `https://api.themoviedb.org/3/tv/${seriesId}/videos?language=en-US`,
@@ -16,7 +20,7 @@ const VideoBackground = ({ seriesId }) => {
     );
 
     const trailer = filteredData.length ? filteredData[0] : json.results[0];
-    console.log(trailer);
+    dispatch(addTrailerVideo(trailer));
   };
 
   useEffect(() => {
