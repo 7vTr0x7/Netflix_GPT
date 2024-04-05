b9be95eee7b2a49f274086e92dac6071ea8b2b9d
7vTr0x7 <7vtrox7@gmail.com>
2024-04-05 18:28:32 +0530

    Buidling Secondary Container

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,7 +1,15 @@
 import React from "react";
+import MovieList from "./MovieList";
+import { useSelector } from "react-redux";
 
 const SecondaryContainer = () => {
-  return <div>SecondaryContainer</div>;
+  const series = useSelector((store) => store.series);
+
+  return (
+    <div>
+      <MovieList title={"Now Airing"} series={series.nowAiringSeries} />
+    </div>
+  );
 };
 
 export default SecondaryContainer;
