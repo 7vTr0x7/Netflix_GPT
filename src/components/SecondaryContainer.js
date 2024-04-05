62ef9485dd65510e10c011ed653416f10e0513c3
7vTr0x7 <7vtrox7@gmail.com>
2024-04-05 21:22:06 +0530

    airing today api

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -10,7 +10,7 @@ const SecondaryContainer = () => {
     <div className="bg-black">
       <div className="relative -mt-52 z-20">
         <MovieList title={"Trending"} series={series.nowAiringSeries} />
-        <MovieList title={"Airing Today"} series={series.nowAiringSeries} />
+        <MovieList title={"Airing Today"} series={series.airingToday} />
         <MovieList title={"On The Air"} series={series.nowAiringSeries} />
         <MovieList title={"Popular"} series={series.nowAiringSeries} />
         <MovieList title={"Top Rated"} series={series.nowAiringSeries} />
