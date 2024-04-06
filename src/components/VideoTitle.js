7ad67514c475541403f0001e064bdbe8ca1b739e
7vTr0x7 <7vtrox7@gmail.com>
2024-04-06 17:19:47 +0530

    adding more translated words

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,20 +1,24 @@
 import React from "react";
+import { lang } from "./../utils/languageConstants";
+import { useSelector } from "react-redux";
 
 const VideoTitle = ({ title, overview }) => {
+  const selectedLang = useSelector((store) => store.config.lang);
+
   return (
     <div className="w-screen aspect-video  pt-[12%] px-24 absolute text-white bg-gradient-to-r from-black">
       <span className="text-[#e50914] font-bold text-6xl bg-gradient-to-t from-black p-1 rounded-md uppercase">
-        {title}
+        {lang[selectedLang].title}
       </span>
       <p className="py-6 pl-10 text-lg w-1/3 uppercase font-[Bebas Neue] ">
-        {overview}
+        {lang[selectedLang].overview}
       </p>
       <p>
         <button className="bg-white text-black font-semibold text-xl py-3 px-12 rounded-md hover:bg-opacity-80 ">
-          ▷ Play
+          ▷ {lang[selectedLang].play}
         </button>
         <button className="bg-gray-500 mx-3 text-white font-semibold text-xl py-3 px-12 bg-opacity-50 hover:bg-opacity-30  rounded-md ">
-          More Info
+          {lang[selectedLang].moreInfo}
         </button>
       </p>
     </div>
--- src/utils/languageConstants.js
+++ src/utils/languageConstants.js
@@ -3,10 +3,20 @@ export const lang = {
     search: "Search",
     searchPlaceholder: "What Would You Like To Watch Today?",
     signOut: "SignOut",
+    title: "Shōgun",
+    overview:
+      "IN JAPAN IN THE YEAR 1600, AT THE DAWN OF A CENTURY-DEFINING CIVIL WAR, LORD YOSHII TORANAGA IS FIGHTING FOR HIS LIFE AS HIS ENEMIES ON THE COUNCIL OF REGENTS UNITE AGAINST HIM, WHEN A MYSTERIOUS EUROPEAN SHIP IS FOUND MAROONED IN A NEARBY FISHING VILLAGE",
+    play: "Play",
+    moreInfo: "More Info",
   },
   ja: {
     search: "検索",
     searchPlaceholder: "今日は何を見たいですか?",
     signOut: "サインアウト",
+    title: "将軍",
+    overview:
+      "1600年の日本、一世紀を決定づけた内戦の黎明期、吉井虎長公は摂政会議の敵が結束して命がけで戦っていたとき、謎のヨーロッパ船が近くの漁場で取り残されているのが発見された。村",
+    play: "遊ぶ",
+    moreInfo: "より詳しい情報",
   },
 };
