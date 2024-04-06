aec7418e53d7bc17ee445e22912af24e9168cf63
7vTr0x7 <7vtrox7@gmail.com>
2024-04-06 16:16:52 +0530

    Building multi Lang feature

---
+++ src/utils/languageConstants.js
@@ -0,0 +1,10 @@
+export const lang = {
+  en: {
+    search: "Search",
+    searchPlaceholder: "What Would You Like To Watch Today?",
+  },
+  ja: {
+    search: "検索",
+    searchPlaceholder: "今日は何を見たいですか?",
+  },
+};
