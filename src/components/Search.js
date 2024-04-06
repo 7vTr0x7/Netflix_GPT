38d10246f2e27b1a61f234d1679bb0e6ebc56188
7vTr0x7 <7vtrox7@gmail.com>
2024-04-06 16:07:44 +0530

    SearchBar

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,14 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import SearchSuggestions from "./SearchSuggestions";
+import { BACKGROUND_IMAGE } from "./../utils/constant";
 
 const Search = () => {
   return (
     <div>
+      <div className="absolute -z-10">
+        <img src={BACKGROUND_IMAGE} alt="background-img" />
+      </div>
       <SearchBar />
       <SearchSuggestions />
     </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,14 +2,14 @@ import React from "react";
 
 const SearchBar = () => {
   return (
-    <div className="pt-[10%] flex justify-center">
+    <div className="pt-[7%] flex justify-center">
       <form className=" w-1/2 grid bg-gradient-to-t from-black grid-cols-12 rounded-xl">
         <input
           type="text"
           className="py-2 px-4 m-4 col-span-9 rounded-lg bg-[#333] text-white"
           placeholder="What Would You Like To Watch Today?"
         />
-        <button className="py-2 px-4 m-4 col-span-3 text-white bg-gradient-to-t uppercase font-bold from-[#e50914] rounded-lg">
+        <button className="py-2 px-4 m-4 col-span-3 text-white uppercase font-bold bg-gradient-to-t  from-[#e50914] rounded-lg">
           Search
         </button>
       </form>
