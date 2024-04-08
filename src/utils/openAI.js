710e29618916353c5b257e97539f9533e313c1a5
7vTr0x7 <7vtrox7@gmail.com>
2024-04-08 20:10:43 +0530

    fix

--- src/utils/openAI.js
+++ src/utils/openAI.js
@@ -1,6 +1,6 @@
 import OpenAI from "openai";
 
 export const openai = new OpenAI({
-  apiKey: process.env.REACT_APP_OPENAI_API, // This is the default and can be omitted
+  apiKey: process.env["REACT_APP_OPENAI_API"], // This is the default and can be omitted
   dangerouslyAllowBrowser: true,
 });
